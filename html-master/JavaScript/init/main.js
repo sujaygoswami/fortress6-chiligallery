732c81247bdfae71fdb2862a4ed60de01e279c52
Sujay Goswami <sujaygoswami@Sujays-Mac-mini.local>
2024-07-09 18:40:40 +0530

    work update dated 09.07.2024 1st push

--- html-master/JavaScript/init/main.js
+++ html-master/JavaScript/init/main.js
@@ -8,6 +8,32 @@ jQuery(document).ready(function(){
               middle: [],
               right: ["slideshow", "download", "thumbs", "close"],
             },
+            items: {
+                download: {
+                    tpl: '<a class="f-button fancy-image-download-button" title="{{DOWNLOAD}}" data-fancybox-download href="javasript:;"><svg><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2M7 11l5 5 5-5M12 4v12"/></svg></a>',
+                  },
+              },
+          },
+          on: {
+            "*": (fancybox) => {
+              // Current slide
+            //   const slide = fancybox.getSlide();
+        
+              // The corresponding trigger element
+              // (i.e. the link that is clicked to launch Fancybox)
+            //   const triggerEl = slide.triggerEl;
+        
+              // Data from the trigger element
+            //   const title = triggerEl.dataset.title;
+        
+            //   alert(title);
+            
+            var CONTENTWIDTH = jQuery('.fancybox__slide.is-selected .fancybox__content').width();
+            console.log(CONTENTWIDTH);
+            jQuery('.fancybox__toolbar').width(CONTENTWIDTH);
+
+
+            },
           },
       });
 
